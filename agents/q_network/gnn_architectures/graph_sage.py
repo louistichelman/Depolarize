89ec952275b0fc8fd28d9daf3b6f9d6dd82d5d4d
Louis Tichelman <louis.tichelman@gmx.de>
2025-06-19 13:47:28 +0200

    update because state is now dictionary

--- agents/q_network/gnn_architectures/graph_sage.py
+++ agents/q_network/gnn_architectures/graph_sage.py
@@ -10,21 +10,21 @@ class GraphSAGE(BaseArchitecture):
         super().__init__(embed_dim, **kwargs)
         self.input_layer = SAGEConv(3, embed_dim)
         self.layers = torch.nn.ModuleList([SAGEConv(embed_dim, embed_dim) for _ in range(num_layers)])
+        self.to(self.device)
 
     def prepare_batch(self, raw_states):
         data_list = []
         for state in raw_states: 
-            G, sigma, tau, l, _ = state
-
-            data = from_networkx(G)
+            
+            data = state["graph_data"]
             n_nodes = data.num_nodes
-
+            
             # --- Build node features ---
-            sigma_tensor = torch.tensor(sigma, dtype=torch.float32, device=self.device).view(-1, 1)
+            sigma_tensor = torch.tensor(state["sigma"], dtype=torch.float32, device=self.device).view(-1, 1)
             tau_tensor = torch.zeros((n_nodes, 1), dtype=torch.float32, device=self.device)
-            if tau is not None:
-                tau_tensor[tau] = 1.0
-            l_tensor = torch.full((n_nodes, 1), fill_value=l, dtype=torch.float32, device=self.device)
+            if state["tau"] is not None:
+                tau_tensor[state["tau"]] = 1.0
+            l_tensor = torch.full((n_nodes, 1), fill_value=state["edges_left"], dtype=torch.float32, device=self.device)
             
             # --- Concatenate node features ---
             data.x = torch.cat([sigma_tensor, tau_tensor, l_tensor], dim=1)
